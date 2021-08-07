f40143b942870e8d21d43d4e4dacec2d4a088cd5
Chance Gallegos <chance.gallegos@gmail.com>
2021-08-07 17:08:58 -0600

    hopefully no binaries

--- space-planet-sandbox/space-planet-sandbox/Game1.cs
+++ space-planet-sandbox/space-planet-sandbox/Game1.cs
@@ -9,6 +9,11 @@ namespace space_planet_sandbox
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Texture2D gorilla;
+        private Texture2D character;
+        Vector2 characterPosition;
+        float speed;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,6 +24,9 @@ namespace space_planet_sandbox
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            characterPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+_graphics.PreferredBackBufferHeight / 2);
+            speed = 100.0f;
 
             base.Initialize();
         }
@@ -28,6 +36,8 @@ namespace space_planet_sandbox
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            gorilla = Content.Load<Texture2D>("morshu");
+            character = Content.Load<Texture2D>("unknown");
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,6 +46,21 @@ namespace space_planet_sandbox
                 Exit();
 
             // TODO: Add your update logic here
+            var kstate = Keyboard.GetState();
+
+            if (kstate.IsKeyDown(Keys.Up))
+                characterPosition.Y -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Down))
+                characterPosition.Y += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Left))
+                characterPosition.X -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Right))
+                characterPosition.X += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            base.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -43,6 +68,18 @@ namespace space_planet_sandbox
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(gorilla, new Vector2(0, 0), Color.White);
+            _spriteBatch.Draw(character,
+                characterPosition,
+                null,
+                Color.White,
+                0f,
+                new Vector2(character.Width / 2, character.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f);
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
