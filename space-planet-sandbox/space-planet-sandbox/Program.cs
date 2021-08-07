3f5a3c70839cf91a766cf5951542b7619da0ef1d
Chance Gallegos <chance.gallegos@gmail.com>
2021-08-07 16:02:14 -0600

    create the project

---
+++ space-planet-sandbox/space-planet-sandbox/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace space_planet_sandbox
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
